0fe271bbe4ee953ef158bed66df5195a223759c6
LidaRevtova <55373057+LidaRevtova@users.noreply.github.com>
2019-11-18 17:21:26 +0700

    italian

---
+++ local_italian.py
@@ -0,0 +1,13 @@
+# Localization file (italian).
+TEXT = 'Inserisci il testo: '
+VOWELS = 'aeiou'
+SENTENCES = 'Proposte: '
+WORD = 'Parole: '
+SYLLABLES ='Sillabe: '
+AVERAGE_SENTENCES = 'Lunghezza media della frase in parole: '
+AVERAGE_WORD = 'Lunghezza media della parola sillababile: '
+INDEX_FLESH = 'Indice di leggibilità a filo: '
+TEXT_EASY = 'Il testo è molto facile da leggere (per gli studenti più giovani).'
+TEXT_SIMPLE = 'Testo semplice (per studenti).'
+TEXT_LITTLE_HARD = "Il testo è un po 'difficile da leggere (per gli studenti)."
+TEXT_DIFFICULT = 'Il testo è difficile da leggere (per i laureati).'
