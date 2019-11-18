652653b59d9d1ac07d63d0bfc7164b24802e4968
LidaRevtova <55373057+LidaRevtova@users.noreply.github.com>
2019-11-18 17:20:10 +0700

    english

---
+++ local_english.py
@@ -0,0 +1,13 @@
+# Localization file (english).
+TEXT = 'Enter the text: '
+VOWELS = 'aeiou'
+SENTENCES = 'Suggestions: '
+WORD = 'Words: '
+SYLLABLES = 'Syllables: '
+AVERAGE_SENTENCES = 'Average sentence length in words: '
+AVERAGE_WORD = 'Average syllable length: '
+INDEX_FLESH = 'Flush readability index: '
+TEXT_EASY = 'The text is very easy to read (for younger students).'
+TEXT_SIMPLE = 'Simple text (for students).'
+TEXT_LITTLE_HARD = 'The text is a little hard to read (for students).'
+TEXT_DIFFICULT = 'The text is hard to read (for graduates).'
